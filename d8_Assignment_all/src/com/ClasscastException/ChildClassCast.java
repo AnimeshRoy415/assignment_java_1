56ee6cf3554c85e0c9686388a24e8bc07d8aef0f
AnniRoy <101575131+AnniRoy@users.noreply.github.com>
2022-05-29 20:32:50 +0530

    d8_update

---
+++ d8_Assignment_all/src/com/ClasscastException/ChildClassCast.java
@@ -0,0 +1,24 @@
+package com.ClasscastException;
+
+public class ChildClassCast extends ClassCastException {
+
+	String Address= "Nandigram";
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassCastException CCS= new ChildClassCast();
+		CCS.name();
+		
+		ChildClassCast CCC= (ChildClassCast)CCS;	// classdowncasting;
+		
+		System.out.println(CCC.Address);
+		
+		System.out.println("When we creat an Child class object refer of Parent class object, we can access Parent class properties And child"
+				+ " class overriden properties but we can't access the child class own properties to access it we have to downcast from parent class"
+				+ "object to child class if we don't downcast it the compiler will throw an error that error is called 'ClassCastException'. Here "
+				+ "in line no. 13 we are downcasting the parent class to childclass.");
+		
+	}
+
+}
